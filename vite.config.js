5159ee8c9cebbff14aae161262fbb9bf1d8e769b
nikitaBaranwal90 <nikita.baranwal@joshtechnologygroup.com>
2025-07-09 16:16:05 +0530

    Base setup: Completed for assignment 1 (#1)
    * NB_RU_00:Test
    
    * Base setup: Completed base setup for assignment 1
    
    * Base setup- Reviews fixed
    
    * Base setup- reviews fixed
    
    * Base setup: Reviews fixed
    
    * Base setup: review fixes
    
    * NB-RU-00 fix: barrel file fix

--- vite.config.js
+++ vite.config.js
@@ -24,6 +24,7 @@ export default defineConfig(() => {
             }),
         ],
         build: {
+            assetsInlineLimit: 0,
             rollupOptions: {
                 output: {
                     chunkFileNames: 'js/[name]-[hash].js',
