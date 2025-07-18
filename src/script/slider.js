cee7fda5426a6587e733b4b1455b34f0547fd2a7
Ubuntu <nikita.baranwal@joshtechnologygroup.com>
2025-07-18 23:59:48 +0530

    NB-RU-06: Review fixes

--- src/script/slider.js
+++ src/script/slider.js
@@ -1,20 +1,13 @@
 import Splide from '@splidejs/splide';
-import '@splidejs/splide/css'; // Default CSS
 
-document.addEventListener('DOMContentLoaded', () => {
-    new Splide('.splide', {
-        type: 'loop',
-        perPage: 1,
-        autoplay: false,
-        pagination: true,
-        arrows: true,
-        classes: {
-            pagination: 'testimonial__pagination',
-            page: 'testimonial__pagination-page',
-            arrows: 'splide__arrows testimonial-arrows',
-            arrow: 'splide__arrow testimonial-arrow',
-            prev: 'splide__arrow--prev testimonial-prev',
-            next: 'splide__arrow--next testimonial-next',
-        },
-    }).mount();
-});
+new Splide('.splide', {
+    type: 'loop',
+    perPage: 1,
+    autoplay: false,
+    pagination: true,
+    arrows: true,
+    classes: {
+        pagination: 'testimonial__pagination',
+        page: 'testimonial__pagination-page',
+    },
+}).mount();
